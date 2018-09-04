b0471742f1e4939f8cca033a86b1248979ea8438
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-09-04 04:04:42 -0300

    Save predictions

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ rn.seed(2264)
 ###################
 
 from utils import load_datasets_filenames, load_experiment_configuration
-from utils import load_dataset, get_voting_clf
+from utils import load_dataset, get_voting_clf, save_predictions
 
 from sklearn.model_selection import StratifiedKFold, train_test_split
 
@@ -18,16 +18,25 @@ if __name__ == "__main__":
 
 	datasets_filenames = load_datasets_filenames()
 	config = load_experiment_configuration()
+	predictions = {}
 
 	for dataset_filename in datasets_filenames:
 		instances, gold_labels = load_dataset(dataset_filename)
 		skfold = StratifiedKFold(n_splits = config["num_folds"],
 			                     shuffle = True)
-		for train_idxs, test_idxs in skfold.split(X=instances, y=gold_labels):
+
+		predictions[dataset_filename] = {}
+
+		for fold, division in enumerate(skfold.split(X=instances, y=gold_labels), 1):
+			train_idxs = division[0]
+			test_idxs = division[1]
 			possible_train_instances = instances.iloc[train_idxs]
 			possible_train_labels = gold_labels.iloc[train_idxs]
 			test_instances = instances.iloc[test_idxs].values
-			test_labels = instances.iloc[test_idxs].values.ravel()
+			test_gold_labels = instances.iloc[test_idxs].values.ravel()
+
+			predictions[dataset_filename][fold] = {}
+			predictions[dataset_filename][fold]["gold_labels"] = test_gold_labels
 			
 			for sampling_percentage in config["sampling_percentages"]:
 				sampled = train_test_split(possible_train_instances,
@@ -38,10 +47,18 @@ if __name__ == "__main__":
 				train_instances = sampled[0].values
 				train_gold_labels = sampled[2].values.ravel()
 
-				for strategy in config["generation_strategies"]:
-					for base_clf in config["base_classifiers"]:
+				preds[dataset_filename][fold][str(sampling_percentage)] = {}
+				subpredictions = predictions[dataset_filename][fold][str(sampling_percentage)]
+
+				for strategy_name, strategy in config["generation_strategies"]:
+					subpredictions[strategy_name] = {}
+
+					for clf_name, base_clf in config["base_classifiers"]:
 					    clf_pool = strategy(base_clf(), config["pool_size"])
 					    clf_pool.fit(train_instances, train_gold_labels)
 
 					    hard_voting_clf = get_voting_clf(clf_pool)
-					    predictions = hard_voting_clf.predict(test_instances)
+					    cur_predictions = hard_voting_clf.predict(test_instances)
+					    subpredictions[strategy_name][clf_name] = cur_predictions
+
+	save_predictions(predictions)
